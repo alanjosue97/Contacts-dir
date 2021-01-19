6645a9031b8a016a8845d83f9c6bc666bb674657
alanjosue97 <33445418+alanjosue97@users.noreply.github.com>
2021-01-19 00:13:59 -0600

    Se agrego la funcion mostrar_menu()

--- contacts_dir.py
+++ contacts_dir.py
@@ -5,17 +5,26 @@ FOLDER = 'contacts/' #carpeta de contactos
 
 def app():
     #revisa si la carpeta existe o no
-    create_directoro() 
+    create_directorio() 
 
+    #Muestra el menu de opciones
+    mostrar_menu()
 
-def create_directoro():
+def mostrar_menu():
+    print('Seleccione del menu lo que desea hacer: ')
+    print('1) Agregar Nuevo Contacto')
+    print('2) Editar Contacto')
+    print('3) Ver Contactos')
+    print('4) Buscar Contacto')
+    print('5) Eliminar Contacto')
+
+def create_directorio():
     if not os.path.exists(FOLDER):    
         #create file
         os.makedirs(FOLDER)           # si no existe creala
-    #este else es opcional
-    else:
-        print('La carpeta ya existee')
-
-
+        #este else es opcional
+    #else:
+        #print('La carpeta ya existee')
+    
 
 app()
